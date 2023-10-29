ba5eb3d4d521ecfd3b7e33c64808a46229407170
willi247 <89020714+willi247@users.noreply.github.com>
2023-10-29 21:18:29 +0300

    Created the scriptS and added the  CMake files
    Also included the dependencies files

---
+++ build_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Example script to build external dependencies
+
+# Install or build your project's external dependencies
+echo "Building external dependencies..."
+
+# Example: Install Python dependencies using pip
+echo "Installing Python dependencies..."
+pip install numpy
